23847d7c2ae02d791de3c828e7f3351c18037135
Gideon Ritter <gideon.ritter@enymo.com>
2023-10-31 15:17:38 +0100

    Initial commit.

---
+++ src/js/index.ts
@@ -0,0 +1,12 @@
+import { requireNotNull } from "@enymo/ts-nullsafe";
+import React, { useContext } from "react";
+
+export function createContext<T>(defaultValue: T) {
+    const Context = React.createContext<T>(defaultValue);
+    return [Context.Provider, () => useContext(Context)];
+}
+
+export function createRequiredContext<T>(reason?: string) {
+    const Context = React.createContext<T | null>(null);
+    return [Context.Provider, () => requireNotNull(useContext(Context), reason)];
+}
