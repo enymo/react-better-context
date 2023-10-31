025e959bf8c78bb5a092dea07fae650fa821ea29
Gideon Ritter <gideon.ritter@enymo.com>
2023-10-31 15:20:59 +0100

    Minor type fix.

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,10 +3,10 @@ import React, { useContext } from "react";
 
 export function createContext<T>(defaultValue: T) {
     const Context = React.createContext<T>(defaultValue);
-    return [Context.Provider, () => useContext(Context)];
+    return [Context.Provider, () => useContext(Context)] as const;
 }
 
 export function createRequiredContext<T>(reason?: string) {
     const Context = React.createContext<T | null>(null);
-    return [Context.Provider, () => requireNotNull(useContext(Context), reason)];
+    return [Context.Provider, () => requireNotNull(useContext(Context), reason)] as const;
 }
