66cdb445039a886538dcd2e6b791da2b9ae283ac
Gideon Ritter <gideon.ritter@enymo.com>
2026-03-05 10:30:05 +0100

    Minor improvements

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,12 @@
 import { requireNotNull } from "@enymo/ts-nullsafe";
-import React, { useContext } from "react";
+import { createContext as originalCreateContext, useContext } from "react";
 
 export function createContext<T>(defaultValue: T) {
-    const Context = React.createContext<T>(defaultValue);
+    const Context = originalCreateContext<T>(defaultValue);
     return [Context.Provider, () => useContext(Context)] as const;
 }
 
-export function createRequiredContext<T>(reason?: string) {
-    const Context = React.createContext<T | null>(null);
-    return [Context.Provider, () => requireNotNull(useContext(Context), reason)] as const;
+export function createRequiredContext<T>(message?: string) {
+    const Context = originalCreateContext<T | null>(null);
+    return [Context.Provider, () => requireNotNull(useContext(Context), message)] as const;
 }
